943e7c21648c79ad73ada0018fd88d3d5bf2bdb2
Alessandro Delgado <adelgado1313@gmail.com>
2016-10-16 01:31:53 +0200

    Take config from the CLI

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,46 @@
 extern crate hyper;
+extern crate clap;
 
 use hyper::*;
 use std::io;
 use std::io::Read;
+use clap::{Arg, App, SubCommand};
+
 
 // fn get_api_endpoint(server_url: String) {
 //     server_url.to_string() + "/api/";
 // }
 
 fn main() {
-    println!("Rocket Chat!");
+    let matches =
+        App::new("Rocket Chat client")
+            .arg(Arg::with_name("server")
+                .required(true)
+                .takes_value(true))
+            .arg(Arg::with_name("username")
+                .required(true)
+                .takes_value(true))
+            .arg(Arg::with_name("password")
+                .required(true)
+                .takes_value(true))
+            .get_matches();
 
-    // println!("Please input the server URL:");
-    // let mut server_url = String::new();
-    // io::stdin().read_line(&mut server_url)
-    //     .expect("Failed to read line");
+    // Calling .unwrap() is safe here because "INPUT" is required (if "INPUT" wasn't
+    // required we could have used an 'if let' to conditionally get the value)
 
-    let server_url : String = "https://chat.creators.co/".to_string();
-
-    // println!("Please input an username:");
-    // let mut user_username = String::new();
-    // io::stdin().read_line(&mut user_username)
-    //     .expect("Failed to read line");
-
-    // println!("Please input the password:");
-    // let mut user_password = String::new();
-    // io::stdin().read_line(&mut user_password)
-    //     .expect("Failed to read line");
+    let server_url = matches.value_of("server").unwrap();
+    let username = matches.value_of("username").unwrap();
+    let password = matches.value_of("password").unwrap();
+    
+    println!("Using server at: {}", &server_url);
+    println!("Using username: {}", matches.value_of("username").unwrap());
+    println!("Using password: {}", matches.value_of("password").unwrap());
 
     // println!("URL: {}, user: {}, password: {}", server_url, user_username, user_password)
 
     let client = Client::new();
     // let endpoint = get_api_endpoint(server_url);
-    let endpoint = server_url + "/api/version";
+    let endpoint = server_url.to_string() + "/api/version";
     let mut res = client.get(&endpoint).send().unwrap();
     assert_eq!(res.status, hyper::Ok);
     let mut s = String::new();
