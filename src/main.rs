77cc57d04cffc7cc1a2e244138055bae5fd103c1
Alessandro Delgado <adelgado1313@gmail.com>
2016-10-16 05:02:43 +0200

    Implement login endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,24 @@
+//#![feature(custom_derive, plugin)]
+//#![plugin(serde_macros)]
+
 extern crate hyper;
 extern crate clap;
+extern crate serde_json;
 
 use hyper::*;
+use hyper::header::{Headers, ContentType};
+use hyper::mime::{Mime, TopLevel, SubLevel};
 use std::io::Read;
 use clap::{Arg, App};
+use serde_json::Value;
 
 fn get_api_endpoint(server_url: &str, method: &str) -> String {
     return server_url.to_string() + "api/" + method;
+}
+
+struct ChatAuth {
+    auth_token: String,
+    user_id: String,
 }
 
 struct ChatClient {
@@ -33,7 +45,35 @@ impl ChatClient {
         println!("{}", s);
     }
 
-}
+    fn login(&self) -> ChatAuth {
+        let client = Client::new();
+        let login_endpoint = get_api_endpoint(&self.url, "login");
+        println!("{}", login_endpoint);
+        let body = format!("user={}&password={}", self.username, self.password);
+        println!("{}", body);
+
+        let mut headers = Headers::new();
+        headers.set(ContentType::form_url_encoded());
+        let mut res = client.post(&login_endpoint)
+            .headers(headers)
+            .body(&body).send().unwrap();
+        assert_eq!(res.status, hyper::Ok);
+        let mut response_content = String::new();
+        res.read_to_string(&mut response_content).unwrap();
+
+        let response_json: Value =
+            serde_json::from_str(&response_content).unwrap();
+         
+        let data = response_json.as_object().unwrap()
+            .get("data").unwrap()
+            .as_object().unwrap();
+
+        return ChatAuth {
+            auth_token: data.get("authToken").unwrap().to_string(),
+            user_id: data.get("userId").unwrap().to_string(),
+        };
+
+    }
 
 
 fn main() {
@@ -62,5 +102,7 @@ fn main() {
 
     client.display_config();
     client.get_api_version();
+    client.login();
+
 
 }
