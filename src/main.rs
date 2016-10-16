00c62dc3f011028d75959de484e4edfbb6c62aea
Alessandro Delgado <adelgado1313@gmail.com>
2016-10-16 02:13:02 +0200

    Start implementing ChatClient

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,36 @@ use hyper::*;
 use std::io::Read;
 use clap::{Arg, App};
 
+fn get_api_endpoint(server_url: &str, method: &str) -> String {
+    return server_url.to_string() + "/api/" + method;
+}
 
-// fn get_api_endpoint(server_url: String) {
-//     server_url.to_string() + "/api/";
-// }
+struct ChatClient {
+    url: String,
+    username: String,
+    password: String,
+}
+
+impl ChatClient {
+
+    fn display_config(&self) {
+        println!("Using server at: {}", self.url);
+        println!("Using username: {}", self.username);
+        println!("Using password: {}", self.password);
+    }
+
+    fn get_api_version(&self) {
+        let client = Client::new();
+        let version_endpoint = get_api_endpoint(&self.url, "version");
+        let mut res = client.get(&version_endpoint).send().unwrap();
+        assert_eq!(res.status, hyper::Ok);
+        let mut s = String::new();
+        res.read_to_string(&mut s).unwrap();
+        println!("{}", s);
+    }
+
+}
+
 
 fn main() {
     let matches =
@@ -24,25 +50,17 @@ fn main() {
                 .takes_value(true))
             .get_matches();
 
-    // Calling .unwrap() is safe here because "INPUT" is required (if "INPUT" wasn't
-    // required we could have used an 'if let' to conditionally get the value)
-
     let server_url = matches.value_of("server").unwrap();
     let username = matches.value_of("username").unwrap();
     let password = matches.value_of("password").unwrap();
     
-    println!("Using server at: {}", &server_url);
-    println!("Using username: {}", matches.value_of("username").unwrap());
-    println!("Using password: {}", matches.value_of("password").unwrap());
+    let client = ChatClient {
+        url: server_url.to_string(),
+        username: username.to_string(),
+        password: password.to_string(),
+    };
 
-    // println!("URL: {}, user: {}, password: {}", server_url, user_username, user_password)
+    client.display_config();
+    client.get_api_version();
 
-    let client = Client::new();
-    // let endpoint = get_api_endpoint(server_url);
-    let endpoint = server_url.to_string() + "/api/version";
-    let mut res = client.get(&endpoint).send().unwrap();
-    assert_eq!(res.status, hyper::Ok);
-    let mut s = String::new();
-    res.read_to_string(&mut s).unwrap();
-    println!("{}", s);
 }
